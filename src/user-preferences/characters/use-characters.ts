0109c78b7d6063eb2e4ded5febce974d21663022
Jon Higger <jonathan.higger@gmail.com>
2025-04-15 15:32:22 -0400

    fixed use characters

--- src/user-preferences/characters/use-characters.ts
+++ src/user-preferences/characters/use-characters.ts
@@ -1,26 +1,23 @@
 import { useQuery } from '@tanstack/vue-query'
-import { computed, ref, type ComputedRef } from 'vue'
+import { computed, ref, watch, type ComputedRef } from 'vue'
 import { getCharacters, type CharacterFiltersPartial } from './get-character'
 import type { Character } from './character.type'
 
 export const useCharacters = () => {
   const filters = ref<CharacterFiltersPartial>({})
-  const filterKeys = computed(() =>
-    Object.entries(filters.value).map(([key, value]) => `${key}-${value}`),
+  const filterKey = computed(() =>
+    Object.entries(filters.value)
+      .map(([key, value]) => `${key}-${value}`)
+      .join('.'),
   )
 
   const query = useQuery({
-    queryKey: ['characters', ...filterKeys.value],
+    queryKey: ['characters', filterKey],
     queryFn: () => getCharacters(filters.value),
-    initialData: [] as Character[],
   })
 
-  const characters: ComputedRef<Character[]> = computed(() => query.data.value)
-
   return {
-    characters,
     filters,
-    isLoading: computed(() => query.isLoading.value),
-    error: computed(() => query.error.value),
+    query,
   }
 }
