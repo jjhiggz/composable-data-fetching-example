dccf1246dff20b410084ad308160d24fd2bc64d0
Jon Higger <jonathan.higger@gmail.com>
2025-04-16 10:54:27 -0400

    use-theme is now completely cleaned up

--- src/user-preferences/use-theme.ts
+++ src/user-preferences/use-theme.ts
@@ -12,7 +12,7 @@ export const computeTheme = (
 }
 
 // separated logic, can be tested
-export const getThemePreference = ({
+export const computeThemePreference = ({
   userData,
   userPreferences,
 }: {
@@ -33,6 +33,20 @@ export const getThemePreference = ({
   }
 }
 
+export const getUpdateThemeDTO = ({
+  theme,
+  existingPreference,
+}: {
+  theme: 'light' | 'dark'
+  existingPreference: UpsertThemePreferenceDTO
+}): UpsertThemePreferenceDTO => {
+  const newTheme = theme === 'light' ? 'dark' : 'light'
+  return {
+    ...existingPreference,
+    preferences: [{ key: 'theme', value: newTheme }],
+  }
+}
+
 type UpsertThemePreferenceDTO = Optional<ThemePreference, 'id'>
 export const useTheme = () => {
   const preferencesQuery = useUserPreferences()
@@ -40,7 +54,7 @@ export const useTheme = () => {
   const { data: userData } = useOptionalUser()
 
   const themePreference: ComputedRef<UpsertThemePreferenceDTO> = computed(() =>
-    getThemePreference({
+    computeThemePreference({
       userData: userData.value,
       userPreferences: preferencesQuery.data.value,
     }),
@@ -49,12 +63,12 @@ export const useTheme = () => {
   const theme = computed(() => computeTheme(themePreference.value))
 
   const toggleTheme = () => {
-    const newTheme = theme.value === 'light' ? 'dark' : 'light'
-    updateUserPreference({
-      ...themePreference.value,
-      group: 'theme',
-      preferences: [{ key: 'theme', value: newTheme }],
-    })
+    updateUserPreference(
+      getUpdateThemeDTO({
+        existingPreference: themePreference.value,
+        theme: theme.value,
+      }),
+    )
   }
 
   return {
